79c77161d698f651b794fccf3bd1c916639ad66d
Byron Sommardahl <byron@acklenavenue.com>
2019-10-15 12:33:37 -0500

    Removing requirement for .enf file. It should only be needed for dev. We will use environment variables in deployed instances.

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 // config.js
 const dotenv = require('dotenv');
 const result = dotenv.config();
-if (result.error) {
-  throw result.error;
-}
 const { parsed: envs } = result;
 module.exports = envs;
