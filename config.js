555aa7ce6b98ae20f37184c6511be65e2307dc0e
Nicolas Jalil <nicolas.jalil@herounit.io>
2019-10-16 11:37:01 -0600

    Fixing typo.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-const _ = require('lodash/each')
+const _each = require('lodash/each')
 const result = dotenv.config();
 
 let envs;
@@ -8,7 +8,7 @@ if (!('error' in result)) {
   envs = result.parsed;
 } else {
   envs = {};
-  _.each(process.env, (value, key) => envs[key] = value);
+  _each(process.env, (value, key) => envs[key] = value);
 }
 console.log(envs)
 
