a93ece230604d7811ee4c3bed1bd53db3a34452c
Nicolas Jalil <nicolas.jalil@herounit.io>
2019-10-17 10:06:39 -0600

    Fixed badge criteria width.

--- src/components/BadgeContent/BadgeContent.js
+++ src/components/BadgeContent/BadgeContent.js
@@ -5,7 +5,7 @@ import ReactMarkdown from 'react-markdown';
 const BadgeContent = props => 
     <div className="container-fluid">
         <div className="row">
-            <div className="col-10 mb-3 offset-4 text-left">
+            <div className="col-5 mb-3 offset-4 text-left">
             <h3>Criteria</h3>
             <ReactMarkdown source={props.criteriaNarrative} />
             <a href={props.criteriaURL}  target="_blank" className="criteria-url-link">View external Criteria URL</a>
