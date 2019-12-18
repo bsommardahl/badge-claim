77166cb1bb40e93f727ce4dabf21625d8d36744a
Emanuel Sosa <emanuelesausosa@gmail.com>
2019-12-18 11:11:43 -0600

    litte fix badge page resizing

--- src/components/BadgeHeader/BadgeHeader.js
+++ src/components/BadgeHeader/BadgeHeader.js
@@ -3,9 +3,9 @@ import './BadgeHeader.css'
 
 const BadgeHeader = props => 
     <div className="badge-summary jumbotron">
-        <div className="container-fluid text-left">
+        <div className="container text-left">
             <div className="row">
-                <div className="badge-sidebar offset-sm-0 offset-md-1 offset-lg-2 text-center pt-5">
+                <div className="badge-sidebar col-12 col-md-3 text-center pt-5">
                     <img className="badge-image img-fluid mb-4" src={props.imageSource} alt="This is a badge"/>
                     <div className="issuer-info">
                         <hi className="h5">
@@ -15,17 +15,24 @@ const BadgeHeader = props =>
                         <a href="https://badgr.io/public/issuers/KhURuSXqSNaxI5tS6YytfA" className="issuer-link">Actually Agile</a>
                     </div>                  
                 </div>
-                <div className="col-3 offset-4">
-                    <h1>{props.badgeName}</h1>
-                </div>
-                <div className={props.display || "col-2"} align="right">
+
+                <div className="col-12 col-md-6" style={{ top: "10rem" }}>
+                    <div className="row">
+                        <div className="col-12">
+                            <h1>{props.badgeName}</h1>
+                        </div>
+                        <div className="badge-summary-container col-12">
+                            <p className="description">{props.badgeDescription}</p>
+                        </div>
+                    </div>
+                </div>                
+
+                <div className={props.display || "col-12 col-md-3"} align="right" style={{ top: "10rem" }}>
                     <p>
                         <a href="" onClick={props.openModal} className="btn btn-primary claim-badge-button btn-lg">Claim badge</a>
                     </p>
                 </div>
-                <div className="badge-summary-container col-4 offset-4">
-                    <p className="description">{props.badgeDescription}</p>
-                </div>
+              
             </div>                    
         </div>
     </div>
