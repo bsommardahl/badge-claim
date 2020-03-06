a92abd6088a24f098308160ab5a72389dd034466
Nakdimon <gjherrera.i@gmail.com>
2020-03-06 15:06:59 -0600

    Added wakeUpDyno util.

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const envs = require('./config')
-const express = require('express')
-const path = require('path')
+const envs = require('./config');
+const express = require('express');
+const path = require('path');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 
-const badgeController = require('./routes/controllers/BadgeController')
-const ClaimBadgeController = require('./routes/controllers/ClaimBadgeController')
-const awardBadgeController = require('./routes/controllers/AwardBadgeController')
+const badgeController = require('./routes/controllers/BadgeController');
+const ClaimBadgeController = require('./routes/controllers/ClaimBadgeController');
+const awardBadgeController = require('./routes/controllers/AwardBadgeController');
+const wakeUpDyno = require('./utils/wakeUpDyno');
 
-axios.defaults.baseURL = envs.BASE_URL
+axios.defaults.baseURL = envs.BASE_URL;
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/badges', badgeController);
 
@@ -22,17 +23,18 @@ app.use('/api/claim', ClaimBadgeController);
 
 app.use('/api/award', awardBadgeController);
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname , "build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-    res.send(`This is the badge claim api: ${process.env.PORT}`)
-})
+  res.send(`This is the badge claim api: ${process.env.PORT}`);
+});
 
 app.listen(PORT, () => {
-    console.log(`App listening to ${PORT}....`)
-    console.log('Press Ctrl+C to quit.')
-})
+  console.log(`App listening to ${PORT}....`);
+  console.log('Press Ctrl+C to quit.');
+  wakeUpDyno(envs.BASE_URL);
+});
