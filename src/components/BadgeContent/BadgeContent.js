6ce3f72ee88afb2307f5744a107baf95f7e32586
Anthony <awrodriguezt@gmail.com>
2020-07-01 09:53:49 -0600

    View badge details in-app
    Co-authored-by: laar96@unitec.edu <laar96@unitec.edu>

--- src/components/BadgeContent/BadgeContent.js
+++ src/components/BadgeContent/BadgeContent.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import './BadgeContent.css'
 import ExternalCriteria from '../ExternalCriteria/ExternalCriteria'
 import AwarderInfo from '../AwarderInfo/AwarderInfo'
 import ReactMarkdown from 'react-markdown'
 
 const BadgeContent = props =>
     <div className="container-fluid">
-        <div className="row">
-            <div className="col-5 mb-3 offset-4 text-left">
+        <div className="row" style={{marginLeft:"8%", marginRight:"8%"}}>
+            <div className="col-8 mb-3 text-left">
                 <h3>Criteria</h3>
                 <ReactMarkdown source={props.criteriaNarrative} />
                 <ExternalCriteria criteriaURL={props.criteriaURL}></ExternalCriteria>
+            </div>
+            <div className="col-4" style={{wordWrap: "break-word"}}>
                 <AwarderInfo tags={props.tags}></AwarderInfo>
             </div>
         </div>
