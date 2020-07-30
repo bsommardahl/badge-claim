4fc1e2ccdba2e4d992d7575441d59cf42931594c
Anthony <awrodriguezt@gmail.com>
2020-07-29 18:06:53 -0600

    Prettier WebhookEngine code
    Co-authored-by: laar96@unitec.edu <laar96@unitec.edu>
    Co-authored-by: Carlos Growth <carlosgarcia_97gr@hotmail.com>

--- src/components/Webhooks/WebhookEngine.js
+++ src/components/Webhooks/WebhookEngine.js
@@ -1,13 +1,16 @@
-import {getWebhooks} from '../../../functions/FirebaseU/FirebaseUtils'
-import axios from 'axios'
+import { getWebhooks } from "../../../functions/FirebaseU/FirebaseUtils";
+import axios from "axios";
 
 export const WebhookFire = (issuer, event, payload) => {
-    getWebhooks().on('value', (snapshot) =>{
-        var webhooks = snapshot.val() ? 
-            Object.entries(snapshot.val()).map(([key,value])=> 
-                key.split(":")[0] === issuer && value.event === event ? 
-                axios.post(value.url, {secret: value.secret, payload: payload}).catch(error=>console.log(error))
-                : console.log()) 
-            : {};
-    })
-}
+  getWebhooks().on("value", (snapshot) => {
+    var webhooks = snapshot.val()
+      ? Object.entries(snapshot.val()).map(([key, value]) =>
+          key.split(":")[0] === issuer && value.event === event
+            ? axios
+                .post(value.url, { secret: value.secret, payload: payload })
+                .catch((error) => console.log(error))
+            : console.log()
+        )
+      : {};
+  });
+};
